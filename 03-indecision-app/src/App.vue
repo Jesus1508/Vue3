1fa7fcb03654606cfd3b2fbd4dd7cdcdc2a9167f
Jesus Solis <iscjesus1508@gmail.com>
2024-09-18 01:04:14 -0600

    Iniciando con la creacipón de Chat en Vue3

--- 03-indecision-app/src/App.vue
+++ 03-indecision-app/src/App.vue
@@ -1,22 +1,8 @@
 
 <template>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <hr />
-    <MyCounter :value="5" />
-
-    <hr />
-
-    <MyCounterScript :value="15" />
-
-    <hr>
-
-    <MyCounterScript :value="2"/>
+  <IndecisionView />
 </template>
 
 <script lang="ts" setup>
-import MyCounter from './components/MyCounter.vue';
-import MyCounterScript from './components/MyCounterScript.vue';
-import MyCounterScript2 from './components/my-counter-script/MyCounterScript2'
+import IndecisionView from './views/indecisionView.vue'
 </script>
