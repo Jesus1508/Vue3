2e862d1c1f98ff308f5e6eda3ba3d68b2977a9b1
Jesus Solis <iscjesus1508@gmail.com>
2024-10-03 21:39:01 -0600

    Continuando con pruebas unitarias sobre los componentes VUE, primeras pruebas

--- 03-indecision-app/src/tests/components/MyCounter.test.js
+++ 03-indecision-app/src/tests/components/MyCounter.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import MyCounter from "@/components/MyCounter.vue"
 import { mount } from "@vue/test-utils"
 
@@ -32,6 +33,43 @@ describe('<MyCounter/>', () => {
         expect(squareLabel.text()).toContain(`Square: ${value * value}`)
         // console.log(wrapper.html())
 
+    })
+    
+    test('increment the counter when +1 button is clicked', async () => {
+
+        const value = 5
+        const wrapper = mount(MyCounter, {
+            props: {
+                value: value,
+            }
+        });
+
+        const [counterLabel, squareLabel] = wrapper.findAll('h3')
+
+        const btnIncrement = wrapper.find('button');
+        await btnIncrement.trigger('click');
+
+        expect(counterLabel.text()).toContain(`Counter: ${value + 1}`)
+        expect(squareLabel.text()).toContain(`Square: ${(value + 1) * (value + 1)}`)
+    })
+
+    test('decrement the counter when -1 button is clicked twice', async () => {
+
+        const value = 5
+        const wrapper = mount(MyCounter, {
+            props: {
+                value: value,
+            }
+        });
+
+        const [counterLabel, squareLabel] = wrapper.findAll('h3')
+        const [,btnDecrement] = wrapper.findAll('button')
+
+        await btnDecrement.trigger('click')
+        await btnDecrement.trigger('click')
+
+        expect(counterLabel.text()).toContain(`Counter: ${value - 2}`)
+        expect(squareLabel.text()).toContain(`Square: ${(value - 2) * (value - 2)}`)
     })
 
 })
