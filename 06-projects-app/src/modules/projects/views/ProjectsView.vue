5a4976797d3415b68b027edfe32d5c30be4ba1ab
Jesus Solis Martinez <iscjesus1508@gmail.com>
2024-10-18 17:50:56 -0600

    Iniciando el manejo de slots en vue

---
+++ 06-projects-app/src/modules/projects/views/ProjectsView.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="overflow-x-auto w-full">
+        <table class="table">
+            <!-- head -->
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>Poyecto</th>
+                    <th>Tareas</th>
+                    <th>Avance</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="hover">
+                    <th>2</th>
+                    <td>Hart Hagerty</td>
+                    <td>Desktop Support Technician</td>
+                    <td>Purple</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <InputModal :open="modalOpen" @close="modalOpen = false"/>
+
+    <fab-button @click="modalOpen = true">
+        <AddCircle />
+    </fab-button>
+</template>
+
+<script setup lang="ts">
+import FabButton from '@/modules/common/components/FabButton.vue';
+import InputModal from '@/modules/common/components/InputModal.vue';
+import AddCircle from '@/modules/common/icons/AddCircle.vue';
+import { ref } from 'vue';
+
+const modalOpen = ref(false);
+</script>
