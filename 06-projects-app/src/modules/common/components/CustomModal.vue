d2a36d29afe2da86f2b626a3f3daeac9f00d3cf2
Jesus Solis Martinez <iscjesus1508@gmail.com>
2024-10-21 13:19:54 -0600

    Finalizando con los primeros pasos con slots en vue

--- 06-projects-app/src/modules/common/components/CustomModal.vue
+++ 06-projects-app/src/modules/common/components/CustomModal.vue
@@ -1,21 +1,37 @@
 
 
 <template>
-    <dialog class="modal">
+    <dialog class="modal" :open="open">
+
         <div class="modal-box">
-            <h3 class="text-lg font-bold">Hello!</h3>
-            <p class="py-4">Press ESC key or click the button below to close</p>
-            <div class="modal-action">
-                <form method="dialog">
-                    <!-- if there is a button in form, it will close the modal -->
-                    <button class="btn">Close</button>
-                </form>
+
+            <div class="border-b border-blue-500">
+                <!-- Header -->
+                <slot name="header"/>
+            </div>
+
+            <div class="my-5">
+                <slot name="body" />
+            </div>
+
+            <div class="border-t border-t-blue-500 pt-2">
+                <slot name="footer" />
             </div>
         </div>
     </dialog>
+    <div
+        v-if="open"
+        class="modal-backdrop fixed top-0 left-0 z-10 bg-black opacity-50 w-screen h-screen">
+    </div>
 
 </template>
 
 <script lang="ts" setup>
+    interface Props{
+        open: boolean;
+    }
 
+    withDefaults(defineProps<Props>(), {
+        open: false
+    })
 </script>
