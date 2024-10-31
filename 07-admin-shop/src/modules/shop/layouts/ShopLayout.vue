d8edb27b37c70cbd253168d3d539ba6e9fda3643
Jesus Solis Martinez <iscjesus1508@gmail.com>
2024-10-31 17:43:04 -0600

    Configurando backend para adminShop y variables de entorno (Configurar Docker Desktop)

---
+++ 07-admin-shop/src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- Header Navbar -->
+  <top-menu />
+
+  <router-view />
+
+  <!-- Footer -->
+  <custom-footer />
+</template>
+
+
+<script setup lang="ts">
+  import { RouterView } from 'vue-router';
+  import CustomFooter from '../components/CustomFooter.vue';
+  import TopMenu from '../components/TopMenu.vue';
+</script>
