56f341ce1f62c5bdceb08c5f4a45209ca4f1cb74
Jesus Solis Martinez <iscjesus1508@gmail.com>
2024-11-13 16:00:25 -0600

    Formularios y Mantenimiento de Productos

--- 07-admin-shop/src/modules/auth/guards/is-authenticated.guard.ts
+++ 07-admin-shop/src/modules/auth/guards/is-authenticated.guard.ts
@@ -3,17 +3,16 @@ import { useAuthStore } from '../stores/auth.store';
 import { AuthStatus } from '../interfaces';
 
 const isAuthenticatedGuard = async (
-    to: RouteLocationNormalized,
-    from: RouteLocationNormalized,
-    next: NavigationGuardNext,
+  to: RouteLocationNormalized,
+  from: RouteLocationNormalized,
+  next: NavigationGuardNext,
 ) => {
-    const authStore = useAuthStore();
+  const authStore = useAuthStore();
 
-    // console.log(to);
-    await authStore.checkAuthStatus();
+  // console.log(to);
+  await authStore.checkAuthStatus();
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-    authStore.authStatus === AuthStatus.Unauthenticated ? next({ name: 'home' }) : next();
+  authStore.authStatus === AuthStatus.Unauthenticated ? next({ name: 'home' }) : next();
 };
 
 export default isAuthenticatedGuard;
